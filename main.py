1f9000a49f71b48c59643e3563ff5e938e8cea12
pashok3d <pavel.tarashkevich@gmail.com>
2024-12-24 16:08:00 +0100

    Add torch dataset and dataloader, add loss, optimizer and initial training loop

--- main.py
+++ main.py
@@ -18,7 +18,15 @@ GPT model structure:
 """
 
 import torch
+import math
 from torch import nn
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
+
+WINDOW_SIZE = 16
+BATCH_SIZE = 4
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Load and preprocess dataset
 with open("dataset/the_dream_room.txt", "r") as f:
@@ -36,11 +44,25 @@ def tokenize(text) -> list[int]:
     return [token_to_id[ch] for ch in text]
 
 
-def get_dummy_batch():
-    return torch.tensor([[1, 2, 3], [4, 5, 6]])
+def decode(token_ids: list[int]) -> str:
+    return "".join([id_to_token[token_id] for token_id in token_ids])
 
 
-device = "cpu"
+class TextDataset(Dataset):
+    def __init__(self, text, context_window_size):
+        self.tokens = tokenize(text)
+
+        self.x = []
+        self.y = []
+        for i in range(len(self.tokens) - context_window_size):
+            self.x.append(self.tokens[i : i + context_window_size])
+            self.y.append(self.tokens[i + 1 : i + context_window_size + 1])
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, idx):
+        return torch.tensor(self.x[idx]), torch.tensor(self.y[idx])
 
 
 class GPTBlock(nn.Module):
@@ -83,6 +105,7 @@ class GPT(nn.Module):
         self, vocab_size: int, max_len: int, embedding_dim: int = 5, blocks_num: int = 2
     ):
         super().__init__()
+        self.vocab_size = vocab_size
         self.embedding_dim = embedding_dim
         self.emb = nn.Embedding(vocab_size, embedding_dim)
         self.pos = nn.Embedding(max_len, embedding_dim)
@@ -90,20 +113,55 @@ class GPT(nn.Module):
             *[GPTBlock(embedding_dim, max_len) for _ in range(blocks_num)]
         )
         self.proj = nn.Linear(embedding_dim, vocab_size)
-        self.proj_softmax = nn.Softmax(dim=-1)
 
-    def forward(self, inputs):
+        self.loss_fn = nn.CrossEntropyLoss()
+
+    def forward(self, inputs, labels=None):
+        B, T = inputs.shape
         embs = self.emb(inputs)
-        pos_embs = self.pos(torch.arange(3).repeat(inputs.shape[0], 1))
+        pos_embs = self.pos(torch.arange(T, device=device))  # (T,C)
         blocks_output = self.blocks(embs + pos_embs)
-        logits = self.proj(blocks_output)
-        output = self.proj_softmax(logits)
-        return output
+        logits = self.proj(blocks_output)  # (B,T,vocab_size)
+        if labels is not None:
+            loss = self.loss_fn(logits.view(-1, self.vocab_size), labels.view(-1))
+            return logits, loss
+        else:
+            return logits, None
 
 
-model = GPT(vocab_size=len(tokens), max_len=3)
+ds = TextDataset(text, WINDOW_SIZE)
+dataloader = DataLoader(ds, batch_size=BATCH_SIZE, shuffle=True)
 
-batch = get_dummy_batch()
-batch.to(device)
+model = GPT(vocab_size=len(tokens), max_len=WINDOW_SIZE)
+model.to(device)
 
-result = model(batch)
+optimizer = torch.optim.AdamW(model.parameters())
+
+
+model.train()
+epoch_loss = 0
+steps_n = 0
+with torch.no_grad():
+    for batch in tqdm(dataloader):
+        input, labels = batch[0].to(device), batch[1].to(device)
+        output, loss = model(input, labels)
+        epoch_loss += loss.item()
+        steps_n += 1
+    avg_loss = epoch_loss / steps_n
+expected_init_loss = -math.log(1 / 74)
+print(f"initial train loss: {avg_loss:.3f}, with expected of {expected_init_loss:.3f}")
+
+for epoch in range(5):
+    model.train()
+    epoch_loss = 0
+    steps_n = 0
+    for batch in tqdm(dataloader):
+        input, labels = batch[0].to(device), batch[1].to(device)
+        output, loss = model(input, labels)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        epoch_loss += loss.item()
+        steps_n += 1
+    avg_loss = epoch_loss / steps_n
+    print(f"epoch {epoch} train loss: {avg_loss:.3f}")
